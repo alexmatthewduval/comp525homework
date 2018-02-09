31c702d74ce17415f282cae8671b9b856ebf0fb8
SantiGR <s.gomezriv@outlook.com>
2018-02-08 23:14:08 -0500

    complete function circle_area

---
+++ h1.py
@@ -0,0 +1,12 @@
+import math
+import random
+
+
+def circle_area(radius):
+    """
+    Calculates the area of a circle with given radius.
+    radius: non-negative float
+    Returns: float
+    """
+    area = math.pi*(radius**2)
+    return area
